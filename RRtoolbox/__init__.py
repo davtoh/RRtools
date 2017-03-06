5fbdd1a669846b137a6317f339d937acb74d53a8
davtoh <davsamirtor@gmail.com>
2017-03-06 09:06:50 -0500

    added cv2 mock for readthedocs.org

--- RRtoolbox/__init__.py
+++ RRtoolbox/__init__.py
@@ -13,3 +13,18 @@ __license__ = "BSD-3-Clause"
 __version__ = '1.0.0a2.post3'
 __email__ = "davsamirtor@gmail.com"
 #__status__ = "Pre-release"
+
+import sys
+import os
+try:
+    import cv2
+except ImportError:
+    # solves ImportError: No module named cv2
+    # this changes the behaviour of the module by mocking cv2
+    # this is done so it can be documented in readthedocs.org and
+    # in the future it will be replaced with binaries to
+    # facilitate users that do not know how to install openCV
+    if os.name == 'nt':
+        sys.modules["cv2"] = ""
+    elif os.name == 'posix':
+        sys.modules["cv2"] = ""
